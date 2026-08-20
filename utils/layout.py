dfd6ceb2d0d53a779773fb586b5e78c591bf1a62
Bhumgara <54769848+Bhumgara@users.noreply.github.com>
2026-08-20 09:45:54 +0100

    Add exclude_sensitive to state assignment sidebar

--- utils/layout.py
+++ utils/layout.py
@@ -86,3 +86,4 @@ def init_sidebar():
                 st.session_state["stats_df"] = stats_df
                 st.session_state["pred_test"] = pred_test
                 st.session_state["y_test"] = y_test
+                st.session_state["exclude_sensitive"] = sensitive
